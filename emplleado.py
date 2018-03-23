6eaf10f6ed51bbcd6fb0d945fe6514a249973755
ElDiegoRinaldi <nico13rinaldi@gmail.com>
2018-03-23 09:46:27 -0300

    Ej4 termin

--- emplleado.py
+++ emplleado.py
@@ -18,7 +18,7 @@ class Empleado(object):
         dia_que_tiene_que_ir = 0
         dia_del_mes = calendar.monthrange(anio, mes)[1]
 
-        for item in range(dia_del_mes):
+        for item in range(1, dia_del_mes + 1):
             fecha = datetime.date(anio, mes, item)
 
             if self.lista_horario[fecha.weekday()]:
@@ -31,6 +31,6 @@ class Empleado(object):
                         break
                 dia_que_tiene_que_ir += 1
 
-        promedio_mes = dias_que_fue / dia_del_mes * 100
+        promedio_mes = dias_que_fue / dia_que_tiene_que_ir * 100
 
         return promedio_mes
